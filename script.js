39e289e43298f8bef14fc57a7e1b1f7e4df5de4e
Erin Utter <erinutter@Erins-MacBook-Pro-6.local>
2020-05-06 13:11:18 -0700

    update

---
+++ script.js
@@ -0,0 +1,14 @@
+// referenced https://www.w3schools.com/html/html5_geolocation.asp
+
+let x = document.getElementById("latitudeLongitude");
+function getLocation(){
+  if(navigator.geolocation){
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else{
+    x.innerHTML = "Sorry, geolocation is not supported by your browser.";
+  }
+}
+
+function showPosition(position) {
+  x.innerHTML = "Your latitude is: " + position.coords.latitude + "<br>Your longitude is: " + position.coords.longitude + "<br>Sure, it's not completely accurate, but it's close enough.";
+}
